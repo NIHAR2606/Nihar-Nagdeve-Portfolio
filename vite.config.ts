489afea9b318c26bafee363c3028c11cee2d9d11
NIHAR2606 <118289214+NIHAR2606@users.noreply.github.com>
2025-09-20 21:10:11 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/NIHARS-DATA-DEN/", // 👈 change this to your repo name
   server: {
     host: "::",
     port: 8080,
